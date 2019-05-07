e038383decbc41c07f16b77bb704a89f78107d3a
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-05-07 18:39:42 +0900

    20190507

---
+++ BeginnersSelection/Daydream.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.next();
+
+    String replaced = s.replace("eraser","").replace("erase","").replace("dreamer","").replace("dream","");
+
+    if (replaced.isEmpty) {
+      System.out.println("YES");
+    } else if (!replaced.isEmpty) {
+      System.out.println("NO");
+    }
+  }
+}
---
+++ BeginnersSelection/Otoshidama.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int count = sc.nextInt();
+    int fakeTotal = sc.nextInt();
+
+    for (int i = 0; i < count + 1 ; i++ ) {
+      for (int j = 0; j < count - i + 1 ; j++) {
+
+        int k = count - i - j;
+
+        int total = i * 10000 + j * 5000 + k * 1000;
+
+        if (total == fakeTotal) {
+          System.out.println(i + " " + j + " " + k);
+          return;
+        }
+      }
+    }
+
+    System.out.println("-1 -1 -1");
+  }
+}
