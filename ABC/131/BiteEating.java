3991f95c0516a089e2c2cbd329714816873f8f9c
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-07-17 18:55:12 +0900

    20190717

---
+++ ABC/131/BiteEating.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc  = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int l = sc.nextInt();
+
+    List<Integer> tastes = new ArrayList<>();
+    for (int i = 1; i <= n; i++) {
+      int taste = l + i - 1;
+      tastes.add(taste);
+    }
+
+    List<Integer> tasteAbs = tastes
+      .stream()
+      .map(t -> Math.abs(t))
+      .collect(Collectors.toList());
+
+    Integer exclude = Collections.min(tasteAbs);
+    Integer excludeIndex = tasteAbs.indexOf(exclude);
+
+    tastes.remove(tastes.get(excludeIndex));
+
+    int sum = tastes.stream().mapToInt(t -> t).sum();
+
+    System.out.println(sum);
+  }
+}
