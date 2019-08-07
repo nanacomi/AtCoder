162d346a62dbe1b88be63f003ad509db193d3fd8
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-08-07 17:35:24 +0900

    20190807

---
+++ ABC/136/Transfer.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int n = a -b;
+
+        if (c - n > 0) {
+            System.out.println(c - n);
+        } else {
+            System.out.println(0);
+        }
+    
+    }
+}
