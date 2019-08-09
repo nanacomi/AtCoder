7d5d3be842da8cba3ed7e58c3c9af2e5534be040
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-08-09 17:31:30 +0900

    20190809

---
+++ ABC/136/UnevenNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (String.valueOf(i).length()%2 == 1) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
