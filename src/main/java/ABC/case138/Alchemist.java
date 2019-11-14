89ccfefc113345a8cc8aad6a2e6dca26b77339b2
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-11-14 09:21:30 +0900

    20191114

--- src/main/java/ABC/case138/Alchemist.java
+++ src/main/java/ABC/case138/Alchemist.java
@@ -1,4 +1,31 @@
 package ABC.case138;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Alchemist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Double> values = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            values.add(sc.nextDouble());
+        }
+
+        Collections.sort(values);
+
+        double result = 0;
+        for (int i = 0; i < n-1; i++) {
+            double c = (values.get(i) + values.get(i+1)) / 2;
+            result += c;
+            values.set(i+1, c);
+        }
+
+        System.out.println(result);
+
+    }
 }
